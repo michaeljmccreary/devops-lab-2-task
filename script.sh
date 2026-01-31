a782d6e0e9b876aac1acd0841369f209d356eea5
Michael McCreary <michaelmccreary95@gmail.com>
2026-01-31 10:58:04 +0000

    Add file permission rules

--- script.sh
+++ script.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# Clean up from previous runs (ignore errors if folders don't exist)
 rm -rf fol_1 fol_2
-
-# Recreate folders
 mkdir -p fol_1 fol_2
 
-# Create files
 touch fol_1/1_1.txt fol_1/1_2.txt fol_1/1_3.txt
 touch fol_2/2_1.txt fol_2/2_2.txt fol_2/2_3.txt
+
+# Step 10: _1.txt and _3.txt -> rw------- (600)
+chmod 600 fol_1/1_1.txt fol_1/1_3.txt fol_2/2_1.txt fol_2/2_3.txt
+
+# Step 11: _2.txt -> rwxrwxrwx (777)
+chmod 777 fol_1/1_2.txt fol_2/2_2.txt
 
